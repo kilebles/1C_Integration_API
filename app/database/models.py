7c9f16c07c97e67df7d84c3c40bded0f1517b36c
Elisey <eliseyblajen@gmail.com>
2025-01-22 00:19:37 +0500

    Create request and models

---
+++ app/database/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime, timezone
+
+Base = declarative_base()
+
+class Task(Base):
+  __tablename__ = "tasks"
+  
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  user_bin = Column(String(12), nullable=False)
+  document_type = Column(String(100), nullable=False)
+  counterparty_bin = Column(String(12), nullable=False)
+  name = Column(String(100), nullable=False)
+  quantity = Column(Float, nullable=False)
+  price = Column(Float, nullable=False)
+  created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
---
+++ app/database/requests.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database.models import Base, Task
+from app.core.config import Config
+
+engine = create_engine(Config.DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+  Base.metadata.create_all(bind=engine)
+  
+def create_task_in_db(session, task_data):
+  new_task = Task(**task_data)
+  session.add(new_task)
+  session.commit()
+  session.refresh(new_task)
+  return new_task
