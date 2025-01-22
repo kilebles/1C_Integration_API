15fa078cac0587517efbda45a3ee3557e6b36436
Elisey <eliseyblajen@gmail.com>
2025-01-22 17:41:59 +0500

    Do invoices for 1C

--- app/endpoints/invoices.py
+++ app/endpoints/invoices.py
@@ -1,21 +1,49 @@
-from fastapi import APIRouter, HTTPException, Header
+import logging
+from fastapi import APIRouter, HTTPException, Header, Depends
+from sqlalchemy.orm import Session
 from app.core.config import Config
+from app.database.models import Task
+from app.database.requests import SessionLocal
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-invoices = {
-    "12345": {"id": "12345", "status": "paid", "amount": 1000.0, "date": "2025-01-18"},
-}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+@router.get("/tasks/{task_id}")
+async def get_task(
+    task_id: int,
+    authorization: str = Header(None),
+    db: Session = Depends(get_db)
+):
+    logger.info(f"Запрос на получение задания ID {task_id}")
+    
+    excepted_token = f"Bearer {Config.API_KEY}"
+    if authorization != excepted_token:
+        logger.warning("Неавторизованный доступ")
+        raise HTTPException(status_code=401, detail="Не авторизован")
+    
+    task = db.query(Task).filter(Task.id == task_id).first()
+    if not task:
+        logger.error(f"Задание с ID {task_id} не найдено")
+        raise HTTPException(status_code=404, detail="Задание не найдено")
+    
+    logger.info(f"Задание с ID {task_id} успешно получено")
+    
+    return {
+        "идентификатор": task.id,
+        "бин_пользователя": task.user_bin,
+        "тип_документа": task.document_type,
+        "бин_контрагента": task.counterparty_bin,
+        "наименование": task.name,
+        "количесвто": task.quantity,
+        "цена": task.price,
+        "создано": task.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+    }
 
-@router.get("/invoice/{invoice_id}")
-async def get_invoice(invoice_id: str, authorization: str = Header(None)):
-    print(f"Received Authorization header: {authorization}")  # Для отладки
-    expected_token = f"Bearer {Config.API_KEY}"
-    if authorization != expected_token:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-
-    invoice = invoices.get(invoice_id)
-    if not invoice:
-        raise HTTPException(status_code=404, detail="Invoice not found")
-
-    return invoice
